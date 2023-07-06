7d89f31b46c1dce952a49d37be8e09ea95e4e01c
JoacoVic <jnvicente17@gmail.com>
2023-07-06 08:48:09 -0300

    Esta tiene que ser

--- ejercicios/01_entrada_salida/ejercicio_01/app.py
+++ ejercicios/01_entrada_salida/ejercicio_01/app.py
@@ -28,7 +28,7 @@ class App(customtkinter.CTk):
 
 
     def btn_mostrar_on_click(self):
-        pass
+        print("Esto no anda, sirve")
         
 
 if __name__ == "__main__":
--- ejercicios/01_entrada_salida/ejercicio_02/app.py
+++ ejercicios/01_entrada_salida/ejercicio_02/app.py
@@ -23,7 +23,7 @@ class App(customtkinter.CTk):
         # configure window
         self.title("UTN FRA")
        
-        self.btn_mostrar = customtkinter.CTkButton(master=self, text="Mostrar", command=self.btn_mostrar_on_click)
+        self.btn_mostrar = customtkinter.CTkButton(master=self, text="revelar", command=self.btn_mostrar_on_click)
         self.btn_mostrar.grid(row=2, pady=20, columnspan=2, sticky="nsew")
 
 
